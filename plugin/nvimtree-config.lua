f14fb8e5357ca492fcc809da27a102078da22992
Arkoprabho <achakraborti1995@gmail.com>
2022-08-16 14:50:56 +0530

    MODIFY: Tree sitter navigation related configs

--- plugin/nvimtree-config.lua
+++ plugin/nvimtree-config.lua
@@ -1,1 +1,21 @@
-require("nvim-tree").setup()
+local tree_cb = require'nvim-tree.config'.nvim_tree_callback
+require("nvim-tree").setup({
+  sort_by = "case_sensitive",
+  view = {
+    adaptive_size = true,
+    mappings = {
+      list = {
+        { key = "u", action = "dir_up" },
+        { key = "s", cb = tree_cb("vsplit") },
+        { key = "v", cb = tree_cb("split") },
+        { key = "t", cb = tree_cb("tabnew") },
+      },
+    },
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
