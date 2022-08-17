66fa8a863f72047ee99046eed0f86538dcbd9334
Arkoprabho <achakraborti1995@gmail.com>
2022-08-17 15:25:34 +0530

    FORMAT FILE

--- lua/settings/init.lua
+++ lua/settings/init.lua
@@ -1,9 +1,9 @@
 local set = vim.opt
-set.foldmethod='expr'
-set.foldexpr='nvim_treesitter#foldexpr()'
+set.foldmethod = "expr"
+set.foldexpr = "nvim_treesitter#foldexpr()"
 set.expandtab = true
 set.number = true
-set.mouse = 'a'
+set.mouse = "a"
 set.smarttab = true
 set.shiftwidth = 4
 set.tabstop = 4
@@ -16,7 +16,7 @@ set.splitbelow = true
 set.splitright = true
 set.wrap = false
 set.scrolloff = 10
-set.fileencoding = 'utf-8'
+set.fileencoding = "utf-8"
 set.termguicolors = true
 
 set.relativenumber = true
