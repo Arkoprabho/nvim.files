f7a4c08c0ee0a1dc328b6dfa07539a62d7b1562b
Arkoprabho <achakraborti1995@gmail.com>
2022-08-17 12:28:26 +0530

    MODIFY: Format on save

--- plugin/null-ls-config.lua
+++ plugin/null-ls-config.lua
@@ -1,4 +1,5 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 if not null_ls_status_ok then
     return
 end
@@ -7,6 +8,20 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 local completion = null_ls.builtins.completion
 null_ls.setup({
+    -- you can reuse a shared lspconfig on_attach callback here
+    on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                    -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                    vim.lsp.buf.formatting_sync()
+                end,
+            })
+        end
+    end,
     sources = {
         formatting.stylua,
         formatting.terraform_fmt,
