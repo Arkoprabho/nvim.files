ff314e44bf50d89c01731b124ede31fa16df72ff
Arkoprabho <achakraborti1995@gmail.com>
2022-08-17 13:37:24 +0530

    FORMAT files

--- lua/packer-plugins.lua
+++ lua/packer-plugins.lua
@@ -1,48 +1,51 @@
-return require('packer').startup(function()
+return require("packer").startup(function()
 	-- Packer can manager itself
-	use 'wbthomason/packer.nvim'
+	use("wbthomason/packer.nvim")
 
 	-- Colorscheme
-    use { "catppuccin/nvim", as = "catppuccin" }
+	use({ "catppuccin/nvim", as = "catppuccin" })
 
-    -- File Explorer
-    use {
-        'kyazdani42/nvim-tree.lua',
-        requires = {
-            'kyazdani42/nvim-web-devicons', -- optional, for file icons
-        },
-    }
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
-        requires = { {'nvim-lua/plenary.nvim'} }
-    }
-    -- Status
-    use 'nvim-lualine/lualine.nvim'
+	-- File Explorer
+	use({
+		"kyazdani42/nvim-tree.lua",
+		requires = {
+			"kyazdani42/nvim-web-devicons", -- optional, for file icons
+		},
+	})
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.0",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
+	-- Status
+	use("nvim-lualine/lualine.nvim")
 
-    -- Bar
-    use 'romgrk/barbar.nvim'
-    -- Development plugins
-    use 'mg979/vim-visual-multi'
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-    }
-    use 'tpope/vim-fugitive'
-    use 'tpope/vim-surround'
+	-- Bar
+	use("romgrk/barbar.nvim")
+	-- Development plugins
+	use("mg979/vim-visual-multi")
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			require("nvim-treesitter.install").update({ with_sync = true })
+		end,
+	})
+	use("tpope/vim-fugitive")
+	use("tpope/vim-surround")
 
-    --- LSP Config
-    use 'neovim/nvim-lspconfig'
-    use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
-    use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
-    use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
-    use 'L3MON4D3/LuaSnip' -- Snippets plugin
-    use 'onsails/lspkind.nvim'
-    use {
-        "AckslD/nvim-neoclip.lua",
-        requires = {
-            {'nvim-telescope/telescope.nvim'},
-            {'kkharji/sqlite.lua', module = 'sqlite'}
-        },
-    }
-    use 'jose-elias-alvarez/null-ls.nvim'
+	--- LSP Config
+	use("neovim/nvim-lspconfig")
+	use("hrsh7th/nvim-cmp") -- Autocompletion plugin
+	use("hrsh7th/cmp-nvim-lsp") -- LSP source for nvim-cmp
+	use("saadparwaiz1/cmp_luasnip") -- Snippets source for nvim-cmp
+	use("L3MON4D3/LuaSnip") -- Snippets plugin
+	use("onsails/lspkind.nvim")
+	use({
+		"AckslD/nvim-neoclip.lua",
+		requires = {
+			{ "nvim-telescope/telescope.nvim" },
+			{ "kkharji/sqlite.lua", module = "sqlite" },
+		},
+	})
+	use("jose-elias-alvarez/null-ls.nvim")
 end)
