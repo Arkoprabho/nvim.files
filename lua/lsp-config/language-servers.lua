931bc6631938b689348e419b0a4ba9c8b131d1f3
Arkoprabho <achakraborti1995@gmail.com>
2022-08-19 13:56:43 +0530

    MODIFY: Change keymap of diagnostic open float

--- lua/lsp-config/language-servers.lua
+++ lua/lsp-config/language-servers.lua
@@ -1,5 +1,5 @@
 local opts = { noremap = true, silent = true }
-vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
+vim.keymap.set("n", "<leader>of", vim.diagnostic.open_float, opts)
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
 vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
