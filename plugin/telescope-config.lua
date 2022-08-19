8051ba13adbe0af33f84113b851be2bf39b2943a
Arkoprabho <achakraborti1995@gmail.com>
2022-08-19 13:38:43 +0530

    MODIFY: Add notifier to telescope

--- plugin/telescope-config.lua
+++ plugin/telescope-config.lua
@@ -2,5 +2,5 @@ local telescope = require("telescope")
 
 telescope.load_extension("neoclip")
 telescope.load_extension("session-lens")
-
+telescope.load_extension("notify")
 telescope.load_extension("bookmarks")
