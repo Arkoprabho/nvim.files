908fbb46a34132f43d3b31efa802d1683f306dd6
Arkoprabho <achakraborti1995@gmail.com>
2022-08-22 18:19:51 +0530

    MODIFY: Telescope config with copy pasta

--- plugin/telescope-config.lua
+++ plugin/telescope-config.lua
@@ -5,3 +5,9 @@ telescope.load_extension("session-lens")
 telescope.load_extension("notify")
 telescope.load_extension("bookmarks")
 telescope.load_extension("projects")
+
+telescope.setup({
+	prompt_prefix = " ",
+	selection_caret = " ",
+	path_display = { "smart" },
+})
