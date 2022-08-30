3652813da2dd9a07fd7e0712377a07054ca2d1f0
Arkoprabho <achakraborti1995@gmail.com>
2022-08-30 13:26:38 +0530

    MODIFY: Change auto session config

--- plugin/auto-session.config.lua
+++ plugin/auto-session.config.lua
@@ -8,7 +8,17 @@ if not l_status_ok then
 	return
 end
 
-local opts = {}
+local opts = {
+	log_level = "error",
+
+	cwd_change_handling = {
+		restore_upcoming_session = true, -- already the default, no need to specify like this, only here as an example
+		pre_cwd_changed_hook = nil, -- already the default, no need to specify like this, only here as an example
+		post_cwd_changed_hook = function() -- example refreshing the lualine status line _after_ the cwd changes
+			require("lualine").refresh() -- refresh lualine so the new session name is displayed in the status bar
+		end,
+	},
+}
 
 vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal"
 
