dde07f11cc8e0c0bc1b93d9ada40d58ccb5fe923
Arkoprabho <achakraborti1995@gmail.com>
2022-09-06 20:14:33 +0530

    MODIFY: Ignore git blame on trouble windows

--- plugin/gitblame-config.lua
+++ plugin/gitblame-config.lua
@@ -1,3 +1,3 @@
 vim.g.gitblame_date_format = "%r"
 vim.g.gitblame_highlight_group = "Comment"
-vim.g.gitblame_ignored_filetypes = { "NvimTree" }
+vim.g.gitblame_ignored_filetypes = { "NvimTree", "Trouble" }
