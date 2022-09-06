6b27159570a7e53a566426bc4637209c32fded9d
Arkoprabho <achakraborti1995@gmail.com>
2022-09-06 21:33:48 +0530

    MODIFY: Add diagnostics signs

--- lua/lsp-config/language-servers.lua
+++ lua/lsp-config/language-servers.lua
@@ -91,3 +91,15 @@ lspconfig["yamlls"].setup({
 		},
 	},
 })
+local icons = require("settings.icons")
+local signs = {
+	Error = icons.diagnostics.Error,
+	Warn = icons.diagnostics.Warning,
+	Hint = icons.diagnostics.Hint,
+	Info = icons.diagnostics.Information,
+}
+
+for type, icon in pairs(signs) do
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
