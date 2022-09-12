18778c84cbafc5810f21ba4459dacb441f796f5e
Arkoprabho <achakraborti1995@gmail.com>
2022-09-12 18:37:27 +0530

    MODIFY: Remove catppuccin theme from tab

--- lua/plugin-config/tabline-config.lua
+++ lua/plugin-config/tabline-config.lua
@@ -1,26 +1,12 @@
 local icons = require("settings.icons")
-local mocha = require("catppuccin.palettes").get_palette("mocha")
 require("bufferline").setup({
-	highlights = require("catppuccin.groups.integrations.bufferline").get({
-		styles = { "italic", "bold" },
-		custom = {
-			all = {
-				fill = { bg = "#000000" },
-			},
-			mocha = {
-				background = { fg = mocha.text },
-			},
-			latte = {
-				background = { fg = "#000000" },
-			},
-		},
-	}),
 	options = {
 		mode = "tabs",
 		numbers = "buffer_id",
 		buffer_close_icon = icons.ui.Close,
 		diagnostics = "nvim_lsp",
 		modified_icon = icons.ui.Circle .. " ",
-		separator_style = { icons.separator.filled_right, icons.separator.filled_right },
+		separator_style = "slant",
+		-- separator_style = { icons.separator.filled_right, icons.separator.filled_right },
 	},
 })
