84da38dd72d5dc511e13be83228934b3d3093736
Arkoprabho <achakraborti1995@gmail.com>
2022-09-23 18:02:11 +0530

    MODIFY: Search in ignored files too

--- lua/plugin-config/telescope-config.lua
+++ lua/plugin-config/telescope-config.lua
@@ -19,7 +19,7 @@ telescope.setup({
 	},
 	pickers = {
 		find_files = {
-			find_command = { "fd", "--type", "f", "--strip-cwd-prefix" },
+			find_command = { "fd", "--type", "f", "--strip-cwd-prefix", "--no-ignore" },
 		},
 	},
 })
