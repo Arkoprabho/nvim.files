bc29e2daef04034cab0a4a8b36aac6e0ef03ad1c
Arkoprabho <achakraborti1995@gmail.com>
2022-10-09 22:23:52 +0530

    MODIFY: Cleanup the plugins and pin them to current versions

--- lua/packer-plugins.lua
+++ lua/packer-plugins.lua
@@ -16,7 +16,10 @@ return packer.startup(function()
 	use("wbthomason/packer.nvim")
 
 	-- Performance
-	use("lewis6991/impatient.nvim")
+	use({
+		"lewis6991/impatient.nvim",
+		commit = "b842e16ecc1a700f62adb9802f8355b99b52a5a6",
+	})
 
 	-- Colorscheme
 	-- use({
@@ -26,7 +29,10 @@ return packer.startup(function()
 	-- 		require("plugin-config.theme-config")
 	-- 	end,
 	-- })
-	use("kyazdani42/nvim-web-devicons")
+	use({
+		"kyazdani42/nvim-web-devicons",
+		commit = "a8cf88cbdb5c58e2b658e179c4b2aa997479b3da",
+	})
 
 	use({
 		"folke/tokyonight.nvim",
@@ -39,6 +45,7 @@ return packer.startup(function()
 
 	use({
 		"folke/which-key.nvim",
+		commit = "6885b669523ff4238de99a7c653d47b081b5506d",
 		config = function()
 			require("plugin-config.which-key-config")
 		end,
@@ -48,6 +55,7 @@ return packer.startup(function()
 	-- Notification
 	use({
 		"rcarriga/nvim-notify",
+		commit = "e7cb3e5f93b1fef6a713dbc182eff98badfc6dd4",
 		config = function()
 			require("plugin-config.neoclip-config")
 		end,
@@ -56,6 +64,7 @@ return packer.startup(function()
 	-- File Explorer
 	use({
 		"kyazdani42/nvim-tree.lua",
+		commit = "875d38e52cc4367bad10e648a906a6bd73b3691c",
 		requires = {
 			"kyazdani42/nvim-web-devicons", -- optional, for file icons
 		},
@@ -74,6 +83,7 @@ return packer.startup(function()
 	-- Status
 	use({
 		"nvim-lualine/lualine.nvim",
+		commit = "edca2b03c724f22bdc310eee1587b1523f31ec7c",
 		config = function()
 			require("looks.lualine.init")
 		end,
@@ -82,7 +92,7 @@ return packer.startup(function()
 	-- Tabline
 	use({
 		"akinsho/bufferline.nvim",
-		tag = "v2.*",
+		tag = "v2.12.*",
 		requires = "kyazdani42/nvim-web-devicons",
 		config = function()
 			require("plugin-config.tabline-config")
@@ -97,20 +107,26 @@ return packer.startup(function()
 	-- Development plugins
 	use({
 		"mfussenegger/nvim-dap",
+		commit = "6b12294a57001d994022df8acbe2ef7327d30587",
 		config = function()
 			require("dap.dap-config")
 		end,
 	})
 	use({
 		"rcarriga/nvim-dap-ui",
+		tag = "v2.1.*",
 		config = function()
 			require("plugin-config.dap-ui-config")
 		end,
 		requires = { "mfussenegger/nvim-dap" },
 	})
-	use("nvim-telescope/telescope-dap.nvim")
+	use({
+		"nvim-telescope/telescope-dap.nvim",
+		commit = "b4134fff5cbaf3b876e6011212ed60646e56f060",
+	})
 	use({
 		"iamcco/markdown-preview.nvim",
+		commit = "02cc3874738bc0f86e4b91f09b8a0ac88aef8e96",
 		run = "cd app && npm install",
 		setup = function()
 			vim.g.mkdp_filetypes = { "markdown" }
@@ -118,9 +134,13 @@ return packer.startup(function()
 		ft = { "markdown" },
 	})
 
-	use("mg979/vim-visual-multi")
+	use({
+		"mg979/vim-visual-multi",
+		commit = "724bd53adfbaf32e129b001658b45d4c5c29ca1a",
+	})
 	use({
 		"numToStr/Comment.nvim",
+		tag = "v0.7.0",
 		config = function()
 			require("plugin-config.comment-config")
 		end,
@@ -136,12 +156,8 @@ return packer.startup(function()
 		end,
 	})
 	use({
-		"tpope/vim-fugitive",
-		opt = true,
-	}) -- Deprecated
-	use({
 		"kylechui/nvim-surround",
-		tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+		tag = "v1.0.0", -- Use for stability; omit to use `main` branch for the latest features
 		config = function()
 			require("plugin-config.surround-config")
 		end,
@@ -150,36 +166,60 @@ return packer.startup(function()
 			{ "nvim-treesitter/nvim-treesitter-textobjects" },
 		},
 	})
-	use({ "tpope/vim-surround", opt = true }) -- Deprecated
 
 	--- LSP Config
 	use({
 		"williamboman/mason.nvim",
+		commit = "13217bbf59ed1e3d7633bd7e8e518b93a77e22c5",
 		config = function()
 			require("plugin-config.mason-config")
 		end,
 	})
-	use({ "WhoIsSethDaniel/lualine-lsp-progress.nvim" })
+	use({
+		"WhoIsSethDaniel/lualine-lsp-progress.nvim",
+		commit = "16380c531efad8131ba0f395bdeb97fa2ae169f4",
+	})
 	use({
 		"folke/trouble.nvim",
+		commit = "929315ea5f146f1ce0e784c76c943ece6f36d786",
 		config = function()
 			require("plugin-config.trouble-config")
 		end,
 	})
-	use("neovim/nvim-lspconfig")
-	use("hrsh7th/cmp-path")
-	use("hrsh7th/nvim-cmp") -- Autocompletion plugin
-	use("hrsh7th/cmp-nvim-lsp") -- LSP source for nvim-cmp
-	use("saadparwaiz1/cmp_luasnip") -- Snippets source for nvim-cmp
+	use({
+		"neovim/nvim-lspconfig",
+		commit = "0a8064eda0c7a4475c4a8ceb39199e975308797b",
+	})
+	use({
+		"hrsh7th/cmp-path",
+		commit = "91ff86cd9c29299a64f968ebb45846c485725f23",
+	})
+	use({
+		"hrsh7th/nvim-cmp",
+		commit = "0e436ee23abc6c3fe5f3600145d2a413703e7272",
+	}) -- Autocompletion plugin
+	use({
+		"hrsh7th/cmp-nvim-lsp",
+		commit = "affe808a5c56b71630f17aa7c38e15c59fd648a8",
+	}) -- LSP source for nvim-cmp
+	use({
+		"saadparwaiz1/cmp_luasnip",
+		commit = "a9de941bcbda508d0a45d28ae366bb3f08db2e36",
+	}) -- Snippets source for nvim-cmp
 	use({
 		"L3MON4D3/LuaSnip",
+		tag = "v1.0.0",
 		config = function()
 			require("plugin-config.snippets-config")
 		end,
 	}) -- Snippets plugin
-	use("onsails/lspkind.nvim")
+	use({
+		"onsails/lspkind.nvim",
+		commit = "c68b3a003483cf382428a43035079f78474cd11e",
+	})
 	use({
 		"AckslD/nvim-neoclip.lua",
+		commit = "73fd2494b181cdad5dca2be23bfefd6e34621fd2",
 		requires = {
 			{ "nvim-telescope/telescope.nvim" },
 			{ "kkharji/sqlite.lua", module = "sqlite" },
@@ -190,6 +230,7 @@ return packer.startup(function()
 	})
 	use({
 		"jose-elias-alvarez/null-ls.nvim",
+		commit = "355f7012b4afd92c0387770f1bea892e74a91b2e",
 		config = function()
 			require("lsp.null-ls.config")
 		end,
@@ -198,6 +239,7 @@ return packer.startup(function()
 	-- Terminal
 	use({
 		"akinsho/toggleterm.nvim",
+		commit = "2a787c426ef00cb3488c11b14f5dcf892bbd0bda",
 		config = function()
 			require("plugin-config.toggleterm-config")
 		end,
@@ -206,12 +248,14 @@ return packer.startup(function()
 	-- Git
 	use({
 		"f-person/git-blame.nvim",
+		commit = "08e75b7061f4a654ef62b0cac43a9015c87744a2",
 		config = function()
 			require("plugin-config.gitblame-config")
 		end,
 	})
 	use({
 		"kdheepak/lazygit.nvim",
+		commit = "9c73fd69a4c1cb3b3fc35b741ac968e331642600",
 	})
 	use({
 		"sindrets/diffview.nvim",
@@ -225,27 +269,35 @@ return packer.startup(function()
 	})
 	use({
 		"lewis6991/gitsigns.nvim",
+		tag = "v0.5",
 		config = function()
 			require("plugin-config.gitsigns-config")
 		end,
 	})
 	use({
 		"lukas-reineke/indent-blankline.nvim",
+		tag = "v2.20.2",
 		config = function()
 			require("plugin-config.indent-blankline-config")
 		end,
 	})
 
 	-- Misc
-	use("superhawk610/ascii-blocks.nvim")
+	use({
+		"superhawk610/ascii-blocks.nvim",
+		commit = "a8a7d99fc52df30d7c4e7664818542fd44839d97",
+		cmd = { "AsciiBlockify" },
+	})
 	use({
 		"gen740/SmoothCursor.nvim",
+		commit = "abc2065f748f346c02bed19b3a075d561b20aa6f",
 		config = function()
 			require("plugin-config.smoothcursor-config")
 		end,
 	})
 	use({
 		"anuvyklack/windows.nvim",
+		commit = "e3a1217976d4ec8d2515cb634dbf5d26cabd46d5",
 		requires = {
 			"anuvyklack/middleclass",
 		},
@@ -255,6 +307,8 @@ return packer.startup(function()
 	})
 	use({
 		"folke/zen-mode.nvim",
+		cmd = { "ZenMode" },
+		commit = "6f5702db4fd4a4c9a212f8de3b7b982f3d93b03c",
 		config = function()
 			require("zen-mode").setup({
 				-- your configuration comes here
@@ -263,26 +317,30 @@ return packer.startup(function()
 			})
 		end,
 	})
-	use("nvim-treesitter/nvim-treesitter-context")
-	use("justinmk/vim-sneak")
 	use({
-		"echasnovski/mini.nvim",
-		branch = "stable",
-		config = function()
-			require("plugin-config.mini-config")
-		end,
+		"nvim-treesitter/nvim-treesitter-context",
+		commit = "c46a8a0a60412a8fe43aa6bd3a01845c46de6bf2",
+	})
+	use({
+		"justinmk/vim-sneak",
+		tag = "1.9",
 	})
 	use({
 		"norcalli/nvim-colorizer.lua",
+		commit = "36c610a9717cc9ec426a07c8e6bf3b3abcb139d6",
 		config = function()
 			require("plugin-config.colorizer-config")
 		end,
 	})
 
 	-- Golang related plugins
-	use("rafamadriz/friendly-snippets")
+	use({
+		"rafamadriz/friendly-snippets",
+		commit = "6cd7469403fd06a3840a1065728d1affe1c23ec8",
+	})
 	use({
 		"leoluz/nvim-dap-go",
+		commit = "c2902bb96c45e872d947d7e174775e652439add4",
 		config = function()
 			require("dap.go-dap-config")
 		end,
