da64a9ad0c218b3738b4bfcc608a4f1da1001aaf
Arkoprabho <achakraborti1995@gmail.com>
2022-10-11 22:31:04 +0530

    MODIFY: Back to thin separators

--- lua/plugin-config/tabline-config.lua
+++ lua/plugin-config/tabline-config.lua
@@ -6,7 +6,7 @@ require("bufferline").setup({
 		buffer_close_icon = icons.ui.Close,
 		diagnostics = "nvim_lsp",
 		modified_icon = icons.ui.Circle .. " ",
-		separator_style = "slant",
+		separator_style = "thin",
 		-- separator_style = { icons.separator.filled_right, icons.separator.filled_right },
 	},
 })
