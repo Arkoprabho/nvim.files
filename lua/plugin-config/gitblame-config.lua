c29971125e90f6e3d1bfbb2b54621babbac10bca
Arkoprabho <achakraborti1995@gmail.com>
2022-11-23 22:39:47 +0530

    MODIFY: Dont display virtual text

--- lua/plugin-config/gitblame-config.lua
+++ lua/plugin-config/gitblame-config.lua
@@ -1,3 +1,4 @@
+vim.g.gitblame_display_virtual_text = false
 vim.g.gitblame_date_format = "%r"
 vim.g.gitblame_highlight_group = "Comment"
 vim.g.gitblame_ignored_filetypes = { "NvimTree", "Trouble", "packer" }
