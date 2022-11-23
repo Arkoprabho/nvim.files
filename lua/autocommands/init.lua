fc53fe2bf3d3f21fd4b24940ba9147a2df6bdc74
Arkoprabho <achakraborti1995@gmail.com>
2022-11-23 22:19:24 +0530

    MODIFY: Disable mini plugins on terminal enter

--- lua/autocommands/init.lua
+++ lua/autocommands/init.lua
@@ -28,3 +28,22 @@ if file_exists(file_name) then
 		nested = true,
 	})
 end
+
+-- Disable mini related plugins for specific file types
+local disable_mini_plugins = function()
+	vim.b.minicursorword_disable = true
+	vim.b.minicomment_disable = true
+	vim.b.miniindentscope_disable = true
+	vim.b.minijump2d_disable = true
+	vim.b.minipairs_disable = true
+	vim.b.minisurround_disable = true
+end
+
+local group = vim.api.nvim_create_augroup("mini", { clear = true })
+vim.api.nvim_create_autocmd("TermOpen", {
+	callback = disable_mini_plugins,
+	group = group,
+	once = true,
+	desc = "Disables the mini related plugins when entering a terminal",
+	nested = true,
+})
