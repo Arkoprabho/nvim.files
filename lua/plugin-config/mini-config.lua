72eb2b1482b11c3f84f327ea39058354a69a3a3b
Arkoprabho <achakraborti1995@gmail.com>
2022-11-23 22:02:03 +0530

    MODIFY: Use mini sessions instead of custom solution

--- lua/plugin-config/mini-config.lua
+++ lua/plugin-config/mini-config.lua
@@ -163,3 +163,25 @@ require("mini.surround").setup({
 	-- see `:h MiniSurround.config`.
 	search_method = "cover",
 })
+
+require("mini.sessions").setup({
+	-- Whether to read latest session if Neovim opened without file arguments
+	autoread = true,
+
+	-- Whether to write current session before quitting Neovim
+	autowrite = true,
+
+	-- Whether to force possibly harmful actions (meaning depends on function)
+	force = { read = false, write = true, delete = false },
+
+	-- Hook functions for actions. Default `nil` means 'do nothing'.
+	hooks = {
+		-- Before successful action
+		pre = { read = nil, write = nil, delete = nil },
+		-- After successful action
+		post = { read = nil, write = nil, delete = nil },
+	},
+
+	-- Whether to print session path after action
+	verbose = { read = true, write = true, delete = true },
+})
