a2028553f8c7c734a4e4abffe18f8d9c97c932ab
Arkoprabho <achakraborti1995@gmail.com>
2022-12-16 21:41:05 +0530

    MODIFY: Do not autowidth by default

--- lua/plugin-config/windows-config.lua
+++ lua/plugin-config/windows-config.lua
@@ -1,2 +1,20 @@
 local windows = require("windows")
-windows.setup()
+windows.setup({
+	autowidth = { --		       |windows.autowidth|
+		enable = false,
+		winwidth = 5, --		        |windows.winwidth|
+		filetype = { --	      |windows.autowidth.filetype|
+			help = 2,
+		},
+	},
+	ignore = { --			  |windows.ignore|
+		buftype = { "quickfix" },
+		filetype = { "NvimTree", "neo-tree", "undotree", "gundo" },
+	},
+	animation = {
+		enable = true,
+		duration = 300,
+		fps = 30,
+		easing = "in_out_sine",
+	},
+})
