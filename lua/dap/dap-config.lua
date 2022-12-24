78cb2db079ef07ce87fc95525dbff01659d0da6d
Arkoprabho <achakraborti1995@gmail.com>
2022-12-24 20:10:39 +0530

    MODIFY: Move dap and language specific configs to after/plugin

--- lua/dap/dap-config.lua
+++
@@ -1,6 +0,0 @@
-local icons = require("globals.icons")
-vim.fn.sign_define("DapBreakpoint", { text = icons.ui.Circle, texthl = "", linehl = "", numhl = "" })
-
-require("dap.csharp").setup()
-require("dap.python")
-require("dap.kotlin").setup()
--- lua/dap/python.lua
+++
@@ -1,1 +0,0 @@
-require("dap-python").setup("~/.local/share/nvim/mason/packages/debugpy/venv/bin/python")
