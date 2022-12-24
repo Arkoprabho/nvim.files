a8bbdb7877548d0bf52b52627ab17315936f2ab7
Arkoprabho <achakraborti1995@gmail.com>
2022-12-24 20:55:06 +0530

    MODIFY: Fix kotlin dap configuration

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -38,16 +38,15 @@ function dap_config.kotlin()
 	}
 
 	-- Configuration
-		{
-			type = "kotlin",
-			name = "launch - kotlin",
-			request = "launch",
-			projectRoot = vim.fn.getcwd() .. "/app",
-			mainClass = function()
-				-- return vim.fn.input("Path to main class > ", "myapp.sample.app.AppKt", "file")
-				return vim.fn.input("Path to main class > ", "", "file")
-			end,
-		},
+	dap.configurations.kotlin = {
+		type = "kotlin",
+		name = "launch - kotlin",
+		request = "launch",
+		projectRoot = vim.fn.getcwd() .. "/app",
+		mainClass = function()
+			-- return vim.fn.input("Path to main class > ", "myapp.sample.app.AppKt", "file")
+			return vim.fn.input("Path to main class > ", "", "file")
+		end,
 	}
 end
 
