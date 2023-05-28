4efaa002e285b2e231f2252a70c62cec8cdd1303
Arkoprabho <achakraborti1995@gmail.com>
2023-05-28 13:13:59 +0530

    MODIFY: Change tabline separator

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -7,7 +7,7 @@ bufferline.setup({
 		buffer_close_icon = icons.ui.Close,
 		diagnostics = "nvim_lsp",
 		modified_icon = icons.ui.Circle .. " ",
-		separator_style = "thin",
+		separator_style = "slant",
 		-- separator_style = { icons.separator.filled_right, icons.separator.filled_right },
 	},
 })
