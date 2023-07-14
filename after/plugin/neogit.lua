85d2a59d82a77bd53bbe82a3b733e6448b07d7c2
Arkoprabho <achakraborti1995@gmail.com>
2023-07-14 11:45:11 +0530

    DELET: Hanging comment line

--- after/plugin/neogit.lua
+++ after/plugin/neogit.lua
@@ -100,7 +100,6 @@ neogit.setup({
 		status = {
 			-- Adds a mapping with "B" as key that does the "BranchPopup" command
 			["B"] = "BranchPopup",
-			-- Removes the default mapping of "s"
 		},
 		-- Modify fuzzy-finder buffer mappings
 		finder = {
