19729821c39e3e4d64e92b6027533329a25e830b
Arkoprabho <achakraborti1995@gmail.com>
2023-11-21 11:22:33 +0530

    ADD: Transparency and it's modifications to neovide

--- init.lua
+++ init.lua
@@ -27,4 +27,25 @@ if vim.g.neovide then
 	vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
 	vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
 	vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+
+	-- Helper function for transparency formatting
+	local alpha = function()
+		local transparency = 0.8
+		return string.format("%x", math.floor((255 * vim.g.neovide_transparency_point) or transparency))
+	end
+	-- Set transparency and background color (title bar color)
+	vim.g.neovide_transparency = 0.0
+	vim.g.neovide_transparency_point = 0.8
+	vim.g.neovide_background_color = "#272822" .. alpha()
+	-- Add keybinds to change transparency
+	local change_transparency = function(delta)
+		vim.g.neovide_transparency_point = vim.g.neovide_transparency_point + delta
+		vim.g.neovide_background_color = "#272822" .. alpha()
+	end
+	vim.keymap.set({ "n", "v", "o" }, "<D-]>", function()
+		change_transparency(0.01)
+	end)
+	vim.keymap.set({ "n", "v", "o" }, "<D-[>", function()
+		change_transparency(-0.01)
+	end)
 end
