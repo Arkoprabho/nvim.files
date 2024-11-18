d870e1984c707dbeb4f03528ccf1f0352064577d
Arkoprabho Chakraborti <achakraborti1995@gmail.com>
2024-11-18 18:48:40 +0530

    ADD: Omnisharp LSP

--- after/plugin/lsp/language-servers.lua
+++ after/plugin/lsp/language-servers.lua
@@ -59,13 +59,14 @@ local servers = {
 	"pyright",
 	"tsserver",
 	"terraformls",
-	"csharp_ls",
+	"omnisharp",
 	"gopls",
 	"dockerls",
 	"kotlin_language_server",
 	"nginx_language_server",
 }
 local lspconfig = require("lspconfig")
+
 lspconfig["gopls"].setup({
 	on_attach = function(client, bufnr)
 		require("lsp_signature").on_attach(client, bufnr)
@@ -183,6 +184,51 @@ local yaml_companion = require("yaml-companion").setup({
 })
 
 lspconfig["yamlls"].setup(yaml_companion)
+
+local HOME = os.getenv("HOME")
+local omnisharp_dll = HOME .. "/.local/share/nvim/mason/packages/omnisharp/OmniSharp.dll"
+require("lspconfig").omnisharp.setup({
+	cmd = { "dotnet", omnisharp_dll },
+	settings = {
+		FormattingOptions = {
+			-- Enables support for reading code style, naming convention and analyzer
+			-- settings from .editorconfig.
+			EnableEditorConfigSupport = true,
+			-- Specifies whether 'using' directives should be grouped and sorted during
+			-- document formatting.
+			OrganizeImports = nil,
+		},
+		MsBuild = {
+			-- If true, MSBuild project system will only load projects for files that
+			-- were opened in the editor. This setting is useful for big C# codebases
+			-- and allows for faster initialization of code navigation features only
+			-- for projects that are relevant to code that is being edited. With this
+			-- setting enabled OmniSharp may load fewer projects and may thus display
+			-- incomplete reference lists for symbols.
+			LoadProjectsOnDemand = nil,
+		},
+		RoslynExtensionsOptions = {
+			-- Enables support for roslyn analyzers, code fixes and rulesets.
+			EnableAnalyzersSupport = nil,
+			-- Enables support for showing unimported types and unimported extension
+			-- methods in completion lists. When committed, the appropriate using
+			-- directive will be added at the top of the current file. This option can
+			-- have a negative impact on initial completion responsiveness,
+			-- particularly for the first few completion sessions after opening a
+			-- solution.
+			EnableImportCompletion = nil,
+			-- Only run analyzers against open files when 'enableRoslynAnalyzers' is
+			-- true
+			AnalyzeOpenDocumentsOnly = nil,
+		},
+		Sdk = {
+			-- Specifies whether to include preview versions of the .NET SDK when
+			-- determining which version to use for project loading.
+			IncludePrereleases = true,
+		},
+	},
+})
+
 require("telescope").load_extension("yaml_schema")
 
 local icons = require("globals.icons")
