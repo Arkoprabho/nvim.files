2e853a36b240ef7ca370cad080cdcb7a31813132
Arkoprabho Chakraborti <achakraborti1995@gmail.com>
2025-08-11 20:39:40 +0530

    MODIFY: Vibe coded my way through this

--- lua/plugins/files/mini_sessions.lua
+++ lua/plugins/files/mini_sessions.lua
@@ -1,7 +1,7 @@
-local sessions =  {
+local sessions = {
   "echasnovski/mini.sessions",
   version = "*",
-  lazy = false, -- must load at startup to restore sessions
+  lazy = false, -- must load at startup
   opts = {
     directory = vim.fn.stdpath("data") .. "/sessions",
     autoread = true,
@@ -11,33 +11,37 @@ local sessions =  {
     local sessions = require("mini.sessions")
     sessions.setup(opts)
 
+    -- Use full path escaped to avoid collisions
     local function session_name()
-      return vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+      -- Replace path separators with '__' for uniqueness
+      return vim.fn.getcwd():gsub("/", "%%"):gsub("\\", "%%")
     end
 
     -- Save session before exiting
     vim.api.nvim_create_autocmd("VimLeavePre", {
       callback = function()
-        sessions.write(session_name(), { force = true })
+        pcall(sessions.write, session_name(), { force = true })
       end,
     })
 
-    -- Restore session on startup if appropriate
+    -- Save session when changing directories
+    vim.api.nvim_create_autocmd("DirChanged", {
+      callback = function()
+        pcall(sessions.write, session_name(), { force = true })
+      end,
+    })
+
+    -- Restore session on startup if no files are passed
     vim.api.nvim_create_autocmd("VimEnter", {
       callback = function()
-        -- Skip if:
-        -- 1. Files were passed as args
-        -- 2. Reading from stdin
-        -- 3. In a git commit/rebase/merge message
         if
-          vim.fn.argc() > 0
-          or vim.fn.line2byte("$") ~= -1 -- stdin check
-          or vim.fn.getcwd():match(".git")
+          vim.fn.argc() > 0 -- files passed
+          or vim.fn.line2byte("$") ~= -1 -- stdin
+          or vim.fn.getcwd():match("%.git") -- inside .git
         then
           return
         end
-
-        sessions.read(session_name(), { force = true })
+        pcall(sessions.read, session_name(), { force = true })
       end,
     })
   end,
