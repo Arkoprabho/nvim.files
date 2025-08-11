72f0aff48af96cac594e7a8dc777984646c23ef6
Arkoprabho Chakraborti <achakraborti1995@gmail.com>
2025-08-11 19:49:04 +0530

    MODIFY: Fix bufferline back to my standards

--- lua/plugins/ui/ui.lua
+++ lua/plugins/ui/ui.lua
@@ -1,3 +1,4 @@
+local icons = require("globals.icons")
 local nvim_devicons = {
     "nvim-tree/nvim-web-devicons",
     commit = "c2599a81ecabaae07c49ff9b45dcd032a8d90f1a",
@@ -43,7 +44,30 @@ local bufferline = {
       { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
       { "[B", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer prev" },
       { "]B", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer next" },
+      { "<leader>1", "<CMD>BufferLineGoToBuffer 1<CR>", desc = "Move to buffer 1" },
+      { "<leader>2", "<CMD>BufferLineGoToBuffer 2<CR>", desc = "Move to buffer 2" },
+      { "<leader>3", "<CMD>BufferLineGoToBuffer 3<CR>", desc = "Move to buffer 3" },
+      { "<leader>4", "<CMD>BufferLineGoToBuffer 4<CR>", desc = "Move to buffer 4" },
+      { "<leader>5", "<CMD>BufferLineGoToBuffer 5<CR>", desc = "Move to buffer 5" },
+      { "<leader>6", "<CMD>BufferLineGoToBuffer 6<CR>", desc = "Move to buffer 6" },
+      { "<leader>7", "<CMD>BufferLineGoToBuffer 7<CR>", desc = "Move to buffer 7" },
+      { "<leader>8", "<CMD>BufferLineGoToBuffer 8<CR>", desc = "Move to buffer 8" },
+      { "<leader>C", "<CMD>tabclose<CR>", desc = "Close tab" },
     },
+    opts = {
+        options = {
+            mode = "tabs",
+            numbers = "ordinal",
+            buffer_close_icon = icons.ui.Close,
+            diagnostics = "nvim_lsp",
+            modified_icon = icons.ui.Circle .. " ",
+            separator_style = "thin",
+            -- separator_style = { icons.separator.filled_right, icons.separator.filled_right },
+        },
+    },
+    config = function(_, opts)
+        require("bufferline").setup(opts)
+    end
 }
 
 return {
