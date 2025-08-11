4ab77d137eaea7fe212ce6d29c977ed8d9ec8142
Arkoprabho Chakraborti <achakraborti1995@gmail.com>
2025-08-11 19:52:57 +0530

    ADD: Keymap to navigate between splits

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,6 @@
+-- Navigating between splits
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left split" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to split below" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to split above" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right split" })
+
