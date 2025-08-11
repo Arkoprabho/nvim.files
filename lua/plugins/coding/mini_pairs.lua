efb195b23cd1e0d3880fbbf32dc59f214b950960
Arkoprabho Chakraborti <achakraborti1995@gmail.com>
2025-08-11 22:43:27 +0530

    ADD: Mini pairs

---
+++ lua/plugins/coding/mini_pairs.lua
@@ -0,0 +1,31 @@
+local pairs = {
+    {
+        "echasnovski/mini.pairs",
+        version = "*",
+        event = "InsertEnter",
+        config = function()
+            require("mini.pairs").setup({
+                modes = { insert = true, command = false, terminal = false },
+                mappings = {
+                    ["("] = { action = "open", pair = "()", neigh_pattern = "[^\\]." },
+                    ["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\]." },
+                    ["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\]." },
+
+                    [")"] = { action = "close", pair = "()", neigh_pattern = "[^\\]." },
+                    ["]"] = { action = "close", pair = "[]", neigh_pattern = "[^\\]." },
+                    ["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
+
+                    ['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^\\].", register = { cr = false } },
+                    ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a\\].", register = { cr = false } },
+                    ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\].", register = { cr = false } },
+
+                    [">"] = { action = "close", pair = "<>", neigh_pattern = "[^\\]." },
+                },
+            })
+        end,
+    },
+}
+
+return {
+    pairs,
+}
