3980e5341414d28b2854dab8b5a22efd7070b520
Arkoprabho Chakraborti <achakraborti1995@gmail.com>
2025-08-11 19:59:07 +0530

    ADD: Spectre

---
+++ lua/plugins/files/spectre.lua
@@ -0,0 +1,14 @@
+local spectre = {
+  "nvim-pack/nvim-spectre",
+  cmd = "Spectre",
+  keys = {
+      { "<leader>sw", function() require("spectre").open_visual({select_word=true}) end, {desc = "Search current word"},
+      { "<leader>sp", function() require("spectre").open_file_search({select_word=true}) end, {desc = "Search on current file"},
+      { "<leader>sr", function() require("spectre").open() end, desc = "Search and Replace (Spectre)" },
+  },
+  dependencies = { "nvim-lua/plenary.nvim" },
+}
+
+return {
+    spectre
+}
