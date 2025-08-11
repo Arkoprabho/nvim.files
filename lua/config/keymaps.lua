f8800474e1173445c99b110f4b1d64f87b980314
Arkoprabho Chakraborti <achakraborti1995@gmail.com>
2025-08-11 23:02:44 +0530

    ADD: New keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,13 @@
+vim.g.mapleader = " "
+local map = vim.keymap.set
 -- Navigating between splits
-vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left split" })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to split below" })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to split above" })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right split" })
+map("n", "<C-h>", "<C-w>h", { desc = "Move to left split" })
+map("n", "<C-j>", "<C-w>j", { desc = "Move to split below" })
+map("n", "<C-k>", "<C-w>k", { desc = "Move to split above" })
+map("n", "<C-l>", "<C-w>l", { desc = "Move to right split" })
 
+-- Navigation
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+-- Splits
+map("n", "<C-w>z", "<Cmd>WindowsMaximize<CR>")
