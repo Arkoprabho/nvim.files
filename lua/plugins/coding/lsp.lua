6cd0e08b5ad7b2fa0bf5ee7458615daf83c34a3c
Arkoprabho Chakraborti <achakraborti1995@gmail.com>
2025-08-11 22:12:01 +0530

    Reapply "ADD: AI Generated LSP code"
    This reverts commit 94b7458177a1099ba594c84c91f3bd98cbafb7b2.

---
+++ lua/plugins/coding/lsp.lua
@@ -0,0 +1,70 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    build = ":MasonUpdate",
+    config = true, -- runs `require("mason").setup()`
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    commit = "7f0bf635082bb9b7d2b37766054526a6ccafdb85",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "neovim/nvim-lspconfig",
+    },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "pyright", "tsserver", "yamlls", "gopls" },
+      })
+
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      local on_attach = function(_, bufnr)
+        local opts = { buffer = bufnr, silent = true }
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+      end
+
+      for _, server in ipairs(require("mason-lspconfig").get_installed_servers()) do
+        lspconfig[server].setup({
+          capabilities = capabilities,
+          on_attach = on_attach,
+        })
+      end
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = true, -- loaded by mason-lspconfig
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "L3MON4D3/LuaSnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
+    end,
+  },
+}
