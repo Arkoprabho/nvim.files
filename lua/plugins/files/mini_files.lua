901d0e5bfb537a9f4a2c51296f645b02c1bfde12
Arkoprabho Chakraborti <achakraborti1995@gmail.com>
2025-08-11 23:51:28 +0530

    FORMAT

--- lua/plugins/files/mini_files.lua
+++ lua/plugins/files/mini_files.lua
@@ -3,42 +3,48 @@ local mini_files = {
     tag = "v0.16.0",
     lazy = true,
     keys = {
-    { "-", function() require("mini.files").open() end, desc = "Open mini.files" },
-  },
+        {
+            "-",
+            function()
+                require("mini.files").open()
+            end,
+            desc = "Open mini.files",
+        },
+    },
     opts = {
         windows = {
-          preview = true,
-          width_focus = 100,
-          width_nofocus = 30,
-          width_preview = 100,
-          height_focus = 20,
-          height_nofocus = 15,
-          border = "rounded",
+            preview = true,
+            width_focus = 100,
+            width_nofocus = 30,
+            width_preview = 100,
+            height_focus = 20,
+            height_nofocus = 15,
+            border = "rounded",
         },
         mappings = {
-            close       = 'q',
-            go_in       = '<CR>',
-            go_in_plus  = '<CR>',
-            go_out      = '-',
-            go_out_plus = 'H',
-            mark_goto   = "'",
-            mark_set    = 'm',
-            reset       = '<BS>',
-            reveal_cwd  = '@',
-            show_help   = 'g?',
-            synchronize = ':w',
-            trim_left   = '<',
-            trim_right  = '>',
+            close = "q",
+            go_in = "<CR>",
+            go_in_plus = "<CR>",
+            go_out = "-",
+            go_out_plus = "H",
+            mark_goto = "'",
+            mark_set = "m",
+            reset = "<BS>",
+            reveal_cwd = "@",
+            show_help = "g?",
+            synchronize = ":w",
+            trim_left = "<",
+            trim_right = ">",
         },
         content = {
-          filter = function(entry)
-            return not entry.name:match("^%.") -- hide dotfiles
-          end
+            filter = function(entry)
+                return not entry.name:match("^%.") -- hide dotfiles
+            end,
         },
         options = {
-          use_as_default_explorer = true,
+            use_as_default_explorer = true,
         },
-      },
+    },
     config = function(_, opts)
         local mf = require("mini.files")
         mf.setup(opts)
@@ -55,17 +61,19 @@ local mini_files = {
 
         -- Manage hidden files
         local show_hidden = false
-        vim.keymap.set('n', 'g.', function()
-          show_hidden = not show_hidden
-          mf.refresh({ content = {
-            filter = show_hidden and nil or function(entry)
-              return not entry.name:match("^%.")
-            end
-          }})
-        end, { desc = 'Toggle hidden files in mini.files' })
+        vim.keymap.set("n", "g.", function()
+            show_hidden = not show_hidden
+            mf.refresh({
+                content = {
+                    filter = show_hidden and nil or function(entry)
+                        return not entry.name:match("^%.")
+                    end,
+                },
+            })
+        end, { desc = "Toggle hidden files in mini.files" })
     end,
 }
 
 return {
-    mini_files
+    mini_files,
 }
