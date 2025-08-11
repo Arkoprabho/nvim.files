6899736421efe162cb4bcaf1839ef95545df5107
Arkoprabho Chakraborti <achakraborti1995@gmail.com>
2025-08-11 23:02:53 +0530

    FORMAT

--- lua/plugins/coding/mini.lua
+++ lua/plugins/coding/mini.lua
@@ -1,29 +1,29 @@
 local pairs = {
-	{
-		"echasnovski/mini.pairs",
-		version = "*",
-		event = "InsertEnter",
-		config = function()
-			require("mini.pairs").setup({
-				modes = { insert = true, command = false, terminal = false },
-				mappings = {
-					["("] = { action = "open", pair = "()", neigh_pattern = "[^\\]." },
-					["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\]." },
-					["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\]." },
+    {
+        "echasnovski/mini.pairs",
+        version = "*",
+        event = "InsertEnter",
+        config = function()
+            require("mini.pairs").setup({
+                modes = { insert = true, command = false, terminal = false },
+                mappings = {
+                    ["("] = { action = "open", pair = "()", neigh_pattern = "[^\\]." },
+                    ["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\]." },
+                    ["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\]." },
 
-					[")"] = { action = "close", pair = "()", neigh_pattern = "[^\\]." },
-					["]"] = { action = "close", pair = "[]", neigh_pattern = "[^\\]." },
-					["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
+                    [")"] = { action = "close", pair = "()", neigh_pattern = "[^\\]." },
+                    ["]"] = { action = "close", pair = "[]", neigh_pattern = "[^\\]." },
+                    ["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
 
-					['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^\\].", register = { cr = false } },
-					["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a\\].", register = { cr = false } },
-					["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\].", register = { cr = false } },
+                    ['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^\\].", register = { cr = false } },
+                    ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a\\].", register = { cr = false } },
+                    ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\].", register = { cr = false } },
 
-					[">"] = { action = "close", pair = "<>", neigh_pattern = "[^\\]." },
-				},
-			})
-		end,
-	},
+                    [">"] = { action = "close", pair = "<>", neigh_pattern = "[^\\]." },
+                },
+            })
+        end,
+    },
 }
 
 local comment = {
@@ -42,44 +42,44 @@ local comment = {
 }
 
 local jump = {
-	"echasnovski/mini.jump2d",
-	version = "*",
-	keys = {
-		{
-			",",
-			function()
-				require("mini.jump2d").start()
-			end,
-			desc = "Jump to visible text",
-		},
-	},
-	config = function()
-		require("mini.jump2d").setup({
-			spotter = nil,
+    "echasnovski/mini.jump2d",
+    version = "*",
+    keys = {
+        {
+            ",",
+            function()
+                require("mini.jump2d").start()
+            end,
+            desc = "Jump to visible text",
+        },
+    },
+    config = function()
+        require("mini.jump2d").setup({
+            spotter = nil,
 
-			-- Characters used for labels of jump spots (in supplied order)
-			labels = "abcdefghijklmnopqrstuvwxyz",
-			allowed_lines = {
-				blank = true, -- Blank line (not sent to spotter even if `true`)
-				cursor_before = true, -- Lines before cursor line
-				cursor_at = true, -- Cursor line
-				cursor_after = true, -- Lines after cursor line
-				fold = true, -- Start of fold (not sent to spotter even if `true`)
-			},
-			allowed_windows = {
-				current = true,
-				not_current = true,
-			},
-			hooks = {
-				before_start = nil, -- Before jump start
-				after_jump = nil, -- After jump was actually done
-			},
-		})
-	end,
+            -- Characters used for labels of jump spots (in supplied order)
+            labels = "abcdefghijklmnopqrstuvwxyz",
+            allowed_lines = {
+                blank = true, -- Blank line (not sent to spotter even if `true`)
+                cursor_before = true, -- Lines before cursor line
+                cursor_at = true, -- Cursor line
+                cursor_after = true, -- Lines after cursor line
+                fold = true, -- Start of fold (not sent to spotter even if `true`)
+            },
+            allowed_windows = {
+                current = true,
+                not_current = true,
+            },
+            hooks = {
+                before_start = nil, -- Before jump start
+                after_jump = nil, -- After jump was actually done
+            },
+        })
+    end,
 }
 
 return {
-	pairs,
-	comment,
-	jump,
+    pairs,
+    comment,
+    jump,
 }
