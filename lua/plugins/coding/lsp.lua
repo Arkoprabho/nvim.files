d7b49ac1b7012521fe971106d0962c22b8fed3bc
Arkoprabho Chakraborti <achakraborti1995@gmail.com>
2025-08-11 22:20:43 +0530

    MODIFY: Select autocomplete on tab

--- lua/plugins/coding/lsp.lua
+++ lua/plugins/coding/lsp.lua
@@ -56,9 +56,28 @@ return {
           end,
         },
         mapping = cmp.mapping.preset.insert({
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        }),
+            ["<Tab>"] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                  cmp.select_next_item()
+                elseif luasnip.expand_or_jumpable() then
+                  luasnip.expand_or_jump()
+                else
+                  fallback()
+                end
+              end, { "i", "s" }),
+
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+
+          ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Press Enter only if explicitly selected
+                }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
           { name = "buffer" },
