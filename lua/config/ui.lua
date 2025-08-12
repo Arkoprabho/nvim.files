5894384c23cbeb95a83db3051276e5c639c2c426
Arkoprabho Chakraborti <achakraborti1995@gmail.com>
2025-08-12 11:08:00 +0530

    MODIFY: Show diagnostics in a floating window

--- lua/config/ui.lua
+++ lua/config/ui.lua
@@ -10,3 +10,10 @@ for type, icon in pairs(signs) do
     local hl = "DiagnosticSign" .. type
     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
+vim.keymap.set("n", "<leader>d", function()
+    vim.diagnostic.open_float(nil, {
+        scope = "line",     -- "line", "cursor" or "buffer"
+        source = "always",  -- show source name (LSP, etc.)
+        border = "rounded", -- nice border
+    })
+end, { desc = "Show diagnostics in popup" })
