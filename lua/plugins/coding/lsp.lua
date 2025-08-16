e4e4c9dd8c4ff7b1a150f65f0c45fcc72cb9cf16
Arkoprabho Chakraborti <achakraborti1995@gmail.com>
2025-08-17 01:52:23 +0530

    MODIFY: Ignore global vim errors

--- lua/plugins/coding/lsp.lua
+++ lua/plugins/coding/lsp.lua
@@ -46,6 +46,17 @@ local nvim_lspconfig = {
     "neovim/nvim-lspconfig",
     config = function()
         local lspconfig = require("lspconfig")
+        lspconfig.lua_ls.setup({
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = {
+                            "vim",
+                        },
+                    },
+                },
+            },
+        })
 
         lspconfig.yamlls.setup({
             settings = {
