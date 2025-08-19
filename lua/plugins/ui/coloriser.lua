6a52a326d175d80b7052a48798fa756c7d63699d
Arkoprabho Chakraborti <achakraborti1995@gmail.com>
2025-08-19 11:48:31 +0530

    FORMAT

--- lua/plugins/ui/coloriser.lua
+++ lua/plugins/ui/coloriser.lua
@@ -1,18 +1,18 @@
 local coloriser = {
-    "norcalli/nvim-colorizer.lua",
-    config = function()
-        require("colorizer").setup({
-            "*",  -- Apply to all filetypes
-        }, {
-            RGB = true, -- Enable RGB color codes
-            RRGGBB = true, -- Enable RRGGBB color codes
-            names = false, -- Disable color names
-            RGBA = true, -- Enable RGBA color codes
-            -- Other options can be added here as needed
-        })
-    end,
+	"norcalli/nvim-colorizer.lua",
+	config = function()
+		require("colorizer").setup({
+			"*", -- Apply to all filetypes
+		}, {
+			RGB = true, -- Enable RGB color codes
+			RRGGBB = true, -- Enable RRGGBB color codes
+			names = false, -- Disable color names
+			RGBA = true, -- Enable RGBA color codes
+			-- Other options can be added here as needed
+		})
+	end,
 }
 
 return {
-    coloriser,
+	coloriser,
 }
