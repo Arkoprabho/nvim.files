273da7787bcae007147adc934c5a7c4f949ec6dd
Arkoprabho Chakraborti <achakraborti1995@gmail.com>
2025-08-21 11:49:38 +0530

    MODIFY: Pin the commmit for copilots

--- lua/plugins/coding/copilot.lua
+++ lua/plugins/coding/copilot.lua
@@ -1,28 +1,30 @@
 local copilot = {
-	"zbirenbaum/copilot.lua",
-	cmd = "Copilot",
-	event = "InsertEnter",
-	config = function()
-		require("copilot").setup({
-			suggestion = {
-				enabled = false,
-				auto_trigger = false,
-				debounce = 500, -- delay in ms before Copilot suggestions show
-			},
-			panel = { enabled = false },
-		})
-	end,
+    "zbirenbaum/copilot.lua",
+    commit = "3fd7b50810ae4cccf8b38e4c509b1608f141a9e9",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+        require("copilot").setup({
+            suggestion = {
+                enabled = false,
+                auto_trigger = false,
+                debounce = 500, -- delay in ms before Copilot suggestions show
+            },
+            panel = { enabled = false },
+        })
+    end,
 }
 
 local copilot_cmp = {
-	"zbirenbaum/copilot-cmp",
-	dependencies = { "zbirenbaum/copilot.lua" },
-	config = function()
-		require("copilot_cmp").setup()
-	end,
+    "zbirenbaum/copilot-cmp",
+    commit = "15fc12af3d0109fa76b60b5cffa1373697e261d1",
+    dependencies = { "zbirenbaum/copilot.lua" },
+    config = function()
+        require("copilot_cmp").setup()
+    end,
 }
 
 return {
-	copilot,
-	copilot_cmp,
+    copilot,
+    copilot_cmp,
 }
