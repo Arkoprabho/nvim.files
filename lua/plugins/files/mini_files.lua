34704e18110b67d11a7e5969e5d66a00d512b936
Arkoprabho Chakraborti <achakraborti1995@gmail.com>
2025-08-25 19:02:29 +0530

    MODIFY: Fix hidden files in mini.files

--- lua/plugins/files/mini_files.lua
+++ lua/plugins/files/mini_files.lua
@@ -110,17 +110,30 @@ local mf = {
 		})
 
 		-- Manage hidden files
-		local show_hidden = false
-		vim.keymap.set("n", "g.", function()
-			show_hidden = not show_hidden
-			mf.refresh({
-				content = {
-					filter = show_hidden and nil or function(entry)
-						return not entry.name:match("^%.")
-					end,
-				},
-			})
-		end, { desc = "Toggle hidden files in mini.files" })
+		local show_dotfiles = true
+
+		local filter_show = function(fs_entry)
+			return true
+		end
+
+		local filter_hide = function(fs_entry)
+			return not vim.startswith(fs_entry.name, ".")
+		end
+
+		local toggle_dotfiles = function()
+			show_dotfiles = not show_dotfiles
+			local new_filter = show_dotfiles and filter_show or filter_hide
+			mf.refresh({ content = { filter = new_filter } })
+		end
+
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "MiniFilesBufferCreate",
+			callback = function(args)
+				local buf_id = args.data.buf_id
+				-- Tweak left-hand side of mapping to your liking
+				vim.keymap.set("n", "g.", toggle_dotfiles, { buffer = buf_id })
+			end,
+		})
 	end,
 }
 
