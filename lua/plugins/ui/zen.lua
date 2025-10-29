94f5a0d12668bf922ea5206522c24b98a259acff
Arkoprabho Chakraborti <achakraborti1995@gmail.com>
2025-10-29 12:22:43 +0530

    ADD: ZenMode plugin

---
+++ lua/plugins/ui/zen.lua
@@ -0,0 +1,15 @@
+local zenmode = {
+	"folke/zen-mode.nvim",
+	commit = "438a59c33763d79e34ceb394e012c3bfd6aa1084",
+	cmd = "ZenMode",
+	opts = {
+		window = {
+			backdrop = 0.6,
+			width = 180,
+		},
+	},
+}
+
+return {
+	zenmode,
+}
