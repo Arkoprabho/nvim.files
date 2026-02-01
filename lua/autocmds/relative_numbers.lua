2fccc50e5d040f884fdb43d4b92d0dbbf255c0dd
Arkoprabho Chakraborti <achakraborti1995@gmail.com>
2026-02-01 16:42:42 +0530

    feat(autocmds): enable relative number for opencode_output filetype
    - Adds a new autocmds/relative_numbers.lua file to toggle relative number on buffers with filetype 'opencode_output'
    - Updates init.lua to load this new autocmd
    - Ensures relative numbering is enabled on enter/focus and disabled on leave/blur for better UX in opencode_output windows

---
+++ lua/autocmds/relative_numbers.lua
@@ -0,0 +1,23 @@
+local augroup = vim.api.nvim_create_augroup("RelativeNumbersOpenCodeOutput", { clear = true })
+
+vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "WinEnter" }, {
+	group = augroup,
+	pattern = "*",
+	callback = function(event)
+		local ft = vim.api.nvim_buf_get_option(event.buf, "filetype")
+		if ft == "opencode_output" then
+			vim.api.nvim_buf_set_option(event.buf, "relativenumber", true)
+		end
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "WinLeave" }, {
+	group = augroup,
+	pattern = "*",
+	callback = function(event)
+		local ft = vim.api.nvim_buf_get_option(event.buf, "filetype")
+		if ft == "opencode_output" then
+			vim.api.nvim_buf_set_option(event.buf, "relativenumber", false)
+		end
+	end,
+})
