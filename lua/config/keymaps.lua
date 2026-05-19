e5ebafac2f935efa7808d7ef6e9211fefe6da457
Arkoprabho Chakraborti <achakraborti1995@gmail.com>
2026-05-19 13:25:49 +0530

    feat(keymaps): mergetool related mappings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,6 +6,11 @@ map("n", "<C-j>", "<C-w>j", { desc = "Move to split below" })
 map("n", "<C-k>", "<C-w>k", { desc = "Move to split above" })
 map("n", "<C-l>", "<C-w>l", { desc = "Move to right split" })
 
+-- Diff related mappings
+map("n", "<leader>R", "<CMD>diffget REMOTE<CR>", { desc = "Pull changes from remote during a split" })
+map("n", "<leader>B", "<CMD>diffget BASE<CR>", { desc = "Pull changes from base during a split" })
+map("n", "<leader>L", "<CMD>diffget LOCAL<CR>", { desc = "Pull changes from local during a split" })
+
 -- Navigation
 map("n", "<C-d>", "<C-d>zz")
 map("n", "<C-u>", "<C-u>zz")
